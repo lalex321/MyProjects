6e569c631c5eb0d6e9fe6b629fd423302f156098
Alexey Leznikov <aleznikov@gmail.com>
2015-03-17 17:34:26 +0300

    start

---
+++ HomeWorks/src/homework150312/LSDSort.java
@@ -0,0 +1,33 @@
+package homework150312;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class LSDSort {
+
+	private static final int MAX = 1000000;
+
+	public static void main(String[] args) {
+		
+		int[] data = generate();
+		
+		long start = System.currentTimeMillis();
+		Arrays.sort(data);
+		long stop = System.currentTimeMillis();
+		
+		System.out.println("Elapsed = " + (stop - start));
+	}
+
+	private static int[] generate() {
+		int[] data = new int[MAX];
+		
+		Random random = new Random();
+		
+		for (int i = 0; i < data.length; i++) {
+			data[i] = random.nextInt(MAX); // 0 - 999999
+		}
+		
+		return data;
+	}
+	
+}
